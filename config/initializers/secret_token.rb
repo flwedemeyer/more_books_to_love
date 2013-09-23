4ada3fec629b80d71b78aa4831afb877dc660206
francescawedemeyer <flwedemeyer@gmail.com>
2013-09-23 15:53:44 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookLove::Application.config.secret_token = '7bcd083f97567380b15548e60d4512c16f54a7959ebfbc5b8170859a780674e4dfec34745275c69bd3fe6564513096c06a8b329e4f3c84bb69a4a4447b996ec5'
